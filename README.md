f50a449d652e73928880178ec49acca6d4e3b1fe
Eitan <eitandelajaille1@upb.edu>
2024-08-02 16:51:40 -0400

    Inicializa el proyecto Triangulos con .gitignore

---
+++ README.md
@@ -0,0 +1,19 @@
+# Proyecto Triángulos
+
+Este proyecto es una aplicación de consola en C# que determina el tipo de triángulo basado en las dimensiones de sus lados.
+
+## Requisitos
+
+- Visual Studio 2022
+- .NET 6.0 SDK (incluido con Visual Studio 2022)
+
+## Instrucciones de Uso
+
+1. Clona el repositorio:
+	git clone https://github.com/EitandlJ/Certificacion-II.git
+	cd CertificacionII/Triangulos
+2. Abre el proyecto en Visual Studio 2022
+3. Ejecuta la aplicacion
+4. Ingresa las longitudes de los lados que se soliciten
+5. La aplicacion le indicara el tipo de triangulo basado a las dimensiones ingresadas
+
---
+++ Triangulos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Triangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la longitud del primer lado del triángulo:");
+            double lado1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la longitud del segundo lado del triángulo:");
+            double lado2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la longitud del tercer lado del triángulo:");
+            double lado3 = Convert.ToDouble(Console.ReadLine());
+
+            if (lado1 <= 0 || lado2 <= 0 || lado3 <= 0)
+            {
+                Console.WriteLine("Las longitudes de los lados deben ser mayores a cero.");
+            }
+            else if (lado1 == lado2 && lado2 == lado3)
+            {
+                Console.WriteLine("El triángulo es equilátero.");
+            }
+            else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3)
+            {
+                Console.WriteLine("El triángulo es isósceles.");
+            }
+            else
+            {
+                Console.WriteLine("El triángulo es escaleno.");
+            }
+        }
+    }
+}
